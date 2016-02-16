94a401f8d0012dcc21c7c1834d20855f0c53e89e
Tom Colarusso <etcolarusso@gmail.com>
2016-02-16 15:04:30 -0500

    Added example directive object

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,6 +6,37 @@ var phonecatDirectives = angular.module('phonecatDirectives', []);
 
 phonecatDirectives.directive("exampleDirective", [function(){
 
+	//Example directive object
+	// var directiveDefinitionObject = {
+	//     priority: 0,
+	//     template: '<div></div>', // or // function(tElement, tAttrs) { ... },
+	//     // or
+	//     // templateUrl: 'directive.html', // or // function(tElement, tAttrs) { ... },
+	//     transclude: false,
+	//     restrict: 'A',
+	//     templateNamespace: 'html',
+	//     scope: false,
+	//     controller: function($scope, $element, $attrs, $transclude, otherInjectables) { ... },
+	//     controllerAs: 'stringIdentifier',
+	//     bindToController: false,
+	//     require: 'siblingDirectiveName', // or // ['^parentDirectiveName', '?optionalDirectiveName', '?^optionalParent'],
+	//     compile: function compile(tElement, tAttrs, transclude) {
+	//       return {
+	//         pre: function preLink(scope, iElement, iAttrs, controller) { ... },
+	//         post: function postLink(scope, iElement, iAttrs, controller) { ... }
+	//       }
+	//       // or
+	//       // return function postLink( ... ) { ... }
+	//     },
+	//     // or
+	//     // link: {
+	//     //  pre: function preLink(scope, iElement, iAttrs, controller) { ... },
+	//     //  post: function postLink(scope, iElement, iAttrs, controller) { ... }
+	//     // }
+	//     // or
+	//     // link: function postLink( ... ) { ... }
+	//   };
+
 	return {
 		restrict: 'E',
 		templateUrl: 'partials/example-directive.html'
