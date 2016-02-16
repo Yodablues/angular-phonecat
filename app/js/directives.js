2f1d9ffa206066ca403569b8cb8b32ff8effec9f
Tom Colarusso <etcolarusso@gmail.com>
2016-02-16 13:49:05 -0500

    Example Directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,13 @@
 'use strict';
 
 /* Directives */
+
+var phonecatDirectives = angular.module('phonecatDirectives', []);
+
+phonecatDirectives.directive("exampleDirective", [function(){
+
+	return {
+		restrict: 'E',
+		templateUrl: 'partials/example-directive.html'
+	}
+}]);
